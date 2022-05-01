b35ece2f1f3c67ae45f41c3683b07f26942e2003
Zhihao Cao <1564758299@qq.com>
2022-05-01 16:04:29 +0800

    Update

--- main2.py
+++ main2.py
@@ -1,62 +1,48 @@
 from scene import Scene
 import taichi as ti
 from taichi.math import *
-
-color1 = (0.6, 0.2, 0.2)
-color2 = (0.2, 0.2, 0.6)
-
-scene = Scene(voxel_edges=0, exposure=10)
+color1 = (0.2, 0.2, 0.2); color2 = (0.6, 0.6, 0.6); center = (1, 29, -39)
+scene = Scene(voxel_edges=0, exposure=7)
 scene.set_floor(-20, (1.0, 1.0, 1.0))
-scene.set_directional_light((-1, 1, -1), 0.1, (0.5, 0.5, 0.5))
-
+scene.set_directional_light((1, 1, 0), 0.1, (0.1, 0.1, 0.1))
 @ti.func
 def make_fix(end, num):
-    for m in range(1, end):
-        for j in range(-3, 4):
-            scene.set_voxel(vec3(41 + m, num, j), 1, vec3(color1))
-
+    for m, j in ti.ndrange((1, end), (-3, 4)):
+        scene.set_voxel(vec3(41 + m, num, j), 1, vec3(color2))
 @ti.func
 def make_square(center, length, color):
     for i in range(-length, length+1):
-        scene.set_voxel(vec3(center[0] + length, center[1], center[2] + i), 1, vec3(color))
-        scene.set_voxel(vec3(center[0] - length, center[1], center[2] + i), 1, vec3(color))
-        scene.set_voxel(vec3(center[0] + i, center[1], center[2] + length), 1, vec3(color))
-        scene.set_voxel(vec3(center[0] + i, center[1], center[2] - length), 1, vec3(color))
-
+        scene.set_voxel(vec3(center[0] + length, center[1], center[2] + i), 1, vec3(color)); scene.set_voxel(vec3(center[0] - length, center[1], center[2] + i), 1, vec3(color))
+        scene.set_voxel(vec3(center[0] + i, center[1], center[2] + length), 1, vec3(color)); scene.set_voxel(vec3(center[0] + i, center[1], center[2] - length), 1, vec3(color))
+@ti.func
+def make_star(center, size):
+    for i, j, k in ti.ndrange((-size, size+1), (-size, size+1), (-size, size+1)):
+        point = ivec3(i, j, k)
+        if point.dot(point) < size*size:
+            scene.set_voxel(vec3(center[0] + i, center[1] + j, center[2] + k), 2, vec3(color1))
 @ti.kernel
 def initialize_voxels():
-    for x in ti.ndrange((-56, 42)):
-        if x != 0 and (x % 14 == 0 or x == 4):
-            continue
-        for i in range(-3, 4):
-            for j in range(-3, 4):
-                scene.set_voxel(vec3(x, i, j), 1, vec3(color1))
-                if x < 0:
-                    scene.set_voxel(vec3(x, -56+i, j), 1, vec3(color1))
+    for x, i, j in ti.ndrange((-3, 42), (-3, 4), (-3, 4)):
+        if x == 28 or x == 13:      continue;
+        if x < 22:                  scene.set_voxel(vec3(x, i, j), 1, vec3(color1));
+        else:                       scene.set_voxel(vec3(x, i, j), 1, vec3(color2));
+    for x, i, j in ti.ndrange((-3, 53), (-3, 4), (-3, 4)):
+        scene.set_voxel(vec3(x, -56 + i, j), 1, vec3(color1))
+    for y, i, j in ti.ndrange((-56, 4), (-3, 4), (-3, 4)):
+        scene.set_voxel(vec3(-3, y, 3), 1, vec3(color1));scene.set_voxel(vec3(-3, y, -3), 1, vec3(color1))
+        scene.set_voxel(vec3(3, y, 3), 1, vec3(color1));scene.set_voxel(vec3(3, y, -3), 1, vec3(color1))
+        if y < -30:                 scene.set_voxel(vec3(49 + i, y, j), 1, vec3(color1));
+    for z, i, j in ti.ndrange((-49, 4), (-3, 4), (-3, 4)):
+        if z == -34 or z == -17:    continue;
+        if z > -23:                 scene.set_voxel(vec3(i, -56 + j, z), 1, vec3(color1));
+        else:                       scene.set_voxel(vec3(i, -56 + j, z), 1, vec3(color2));
+    for i, j in ti.ndrange((3, 30), (-4, 5)):
+        scene.set_voxel(vec3(1 + j, i, -i - 1), 1, vec3(color1))
+        scene.set_voxel(vec3(1 + j, i, -i - 2), 1, vec3(color1))
     make_fix(7, 3); make_fix(6, 2); make_fix(5, 1); make_fix(4, 0); make_fix(3, -1); make_fix(2, -2)
-    for y in ti.ndrange((-56, 4)):
-        scene.set_voxel(vec3(-55, y, 3), 1, vec3(color1))
-        scene.set_voxel(vec3(-55, y, -3), 1, vec3(color1))
-        scene.set_voxel(vec3(-48, y, 3), 1, vec3(color1))
-        scene.set_voxel(vec3(-48, y, -3), 1, vec3(color1))
-        if y % 15 == 0 and y < 50:
-            continue
-        for i in range(-3, 4):
-            for j in range(-3, 4):
-                scene.set_voxel(vec3(i, y, j), 1, vec3(color1))
-    for z in ti.ndrange((-49, 4)):
-        if z == -34 or z == -19:
-            continue
-        for i in range(-3, 4):
-            for j in range(-3, 4):
-                scene.set_voxel(vec3(i, -56+j, z), 1, vec3(color1))
-    for i in range(3, 40):
-        for j in range(-6, 7):
-            scene.set_voxel(vec3(-49+j, i, -i-1), 1, vec3(color1))
-            scene.set_voxel(vec3(-49+j, i, -i-2), 1, vec3(color1))
-    make_square((-49, 39, -49), 7, color1); make_square((-49, 39, -49), 6, color2); make_square((-49, 39, -49), 5, color1)
-    make_square((-49, 39, -49), 4, color2); make_square((-49, 39, -49), 3, color1); make_square((-49, 39, -49), 2, color2)
-    make_square((-49, 39, -49), 1, color2); make_square((-49, 39, -49), 0, color1)
+    make_square(center, 7, color1); make_square(center, 6, color2); make_square(center, 5, color1); make_square(center, 4, color2)
+    make_square(center, 3, color1); make_square(center, 2, color2); make_square(center, 1, color2); make_square(center, 0, color1)
+    make_star((center[0], center[1] + 6, center[2]), 3)
 
 initialize_voxels()
 scene.finish()
